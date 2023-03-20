2d2a239f795568685476bf65cbfd8edf1f65720d
«Vitaly» <vitalik.yakhimovich@mail.ru>
2023-03-20 18:11:04 +0300

    add Example 001

---
+++ Example_001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное числои на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трёхзначное число");
+int num = int.Parse(Console.ReadLine());
+
+int result = num / 10 % 10;
+Console.WriteLine(result);
